33cc6b73cad4facae03615b406284ea27ddf1f04
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2025-12-15 22:08:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <div>
+        <h1 className='text-3xl underline'>Welcome to my 3D Portfolio</h1>
+      </div>
+    </main> 
+  );
+}
+
+export default App;
