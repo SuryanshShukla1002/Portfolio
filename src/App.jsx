df1e04e6d03d3d481fb4205ecbecdcf5906d3949
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2026-01-02 15:50:27 +0530

    Showcase section rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import "./App.css";
 
 import Hero from "./sections/Hero";
+import ShowCaseSection from "./sections/ShowCaseSection";
 
 function App() {
   return (
     <>
       <Hero />
+      <ShowCaseSection />
     </>
   );
 }
