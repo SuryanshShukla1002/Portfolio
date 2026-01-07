cff71359af7da97c6448c7ed1f4e7f3bff5754f1
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2026-01-07 23:43:21 +0530

    All compnents and section rendered

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@
 
 import LogoSection from "./components/LogoSection";
 import Navbar from "./components/Navbar";
+import Contact from "./sections/Contact";
 import FeatureCards from "./sections/FeatureCards";
+import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
 import ShowCaseSection from "./sections/ShowCaseSection";
 import TechStack from "./sections/TechStack";
@@ -15,6 +17,8 @@ function App() {
       <ShowCaseSection />
       <FeatureCards />
       <TechStack />
+      <Contact />
+      <Footer />
     </>
   );
 }
